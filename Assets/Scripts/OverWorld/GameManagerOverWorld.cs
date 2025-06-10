77cd7bc1f615814aaca5007892334c8a7cd3168f
Hayes Geldmacher <42843199+HayesGeldmacher@users.noreply.github.com>
2025-06-10 10:52:35 -0600

    Some tweaks and additions

--- Assets/Scripts/OverWorld/GameManagerOverWorld.cs
+++ Assets/Scripts/OverWorld/GameManagerOverWorld.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class GameManagerOverWorld : MonoBehaviour
 {
+    public bool paused = false;
+    [SerializeField] private GameObject _puaseMenu;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        paused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Pause"))
+        {
+            paused = !paused;
+            Pause(paused);
+        }
+    }
+
+    private void Pause(bool freeze)
+    {
+        if (freeze)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+
+        _puaseMenu.SetActive(freeze);
+
     }
 }
